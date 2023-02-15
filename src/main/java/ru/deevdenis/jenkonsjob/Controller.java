4c86a8977bf075ae2c35120e19f1616e56597ee7
Denis Deev <hdsp0ler@gmail.com>
2023-02-15 17:29:40 +0700

    add checkstyle controller

--- src/main/java/ru/deevdenis/jenkonsjob/Controller.java
+++ src/main/java/ru/deevdenis/jenkonsjob/Controller.java
@@ -1,5 +1,6 @@
 package ru.deevdenis.jenkonsjob;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,5 +16,23 @@ public class Controller {
     @RequestMapping("/test")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("Hello from test!");
+    }
+
+    @RequestMapping("/test")
+    public ResponseEntity<String> checkstylePlugin() {
+        int a;
+
+        // test checkstyle
+
+        Object o = new Object();
+
+        // test checkstyle
+
+
+
+
+
+
+        return ResponseEntity.ok("Hello from checkstylePlugin!");
     }
 }
